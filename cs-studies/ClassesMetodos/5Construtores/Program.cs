c41a40064d4cb826fc80d959337b3d404139ba92
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-10 10:49:02 -0300

    novos tópicos

---
+++ cs-studies/ClassesMetodos/5Construtores/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Construtores ## \n");
+
+
+
+
+public class Aluno
+{
+	public Aluno(string? nome) //construtor
+	{
+
+    }
+
+	public string? nome;
+	public int idade;
+	public string? sexo;
+	public string? aprovado;
+} 
