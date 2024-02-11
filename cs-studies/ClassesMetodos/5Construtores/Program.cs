178909a0713a9f9566731dacb63e4ab518a2525c
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-11 19:33:11 -0300

    update

--- cs-studies/ClassesMetodos/5Construtores/Program.cs
+++ cs-studies/ClassesMetodos/5Construtores/Program.cs
@@ -1,15 +1,25 @@
 ﻿Console.WriteLine("## Construtores ## \n");
 
 
+Aluno aluno1 = new Aluno("matheus", 25, "masculino", "aprovado");
+
+System.Console.WriteLine(aluno1.nome);
+System.Console.WriteLine(aluno1.idade);
+System.Console.WriteLine(aluno1.sexo);
+System.Console.WriteLine(aluno1.aprovado);
 
 
 public class Aluno
-{
-	public Aluno(string? nome) //construtor
+{	
+	public Aluno(string alunoNome, int alunoIdade, string alunoSexo, string alunoAprovado) //construtor
 	{
+		nome = alunoNome;
+		idade = alunoIdade;
+		sexo = alunoSexo;
+		aprovado = alunoAprovado;
 
     }
-
+	
 	public string? nome;
 	public int idade;
 	public string? sexo;
