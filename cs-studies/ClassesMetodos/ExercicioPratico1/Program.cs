604b8ed71bc822102e1523d7311e2e358977cc44
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-12 17:51:26 -0300

    update

---
+++ cs-studies/ClassesMetodos/ExercicioPratico1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Car chevrolet = new Car("Sedan", "Chevrolet", "Onix", 2016, 110);
+Car ford = new Car("SUV", "Ford", "EcoSport", 2018, 120);
+
+chevrolet.SpeedUp(chevrolet.Brand);
+ford.SpeedUp(ford.Brand);
+
+public class Car
+{
+    public string Model;
+    public string Automaker;
+    public string Brand;
+    public int Year;
+    public int Power;
+    
+    public Car(string model, string automaker, string brand, int ano, int power)
+    {
+        this.Model = model;
+        this.Automaker = automaker;
+        this.Brand = brand;
+        this.Year = ano;
+        this.Power = power;
+    }
+
+    public void SpeedUp(string brand)
+    {
+        Console.WriteLine($"Speeding up my {brand}");
+    }
+}
