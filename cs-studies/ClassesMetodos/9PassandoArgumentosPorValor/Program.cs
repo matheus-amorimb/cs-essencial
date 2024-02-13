b153ee98142f32fe62a899a453c05c4eaae52109
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-13 13:04:36 -0300

    update

---
+++ cs-studies/ClassesMetodos/9PassandoArgumentosPorValor/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int x = 20;
+
+Console.WriteLine("Valor do argumento x antes de passar por valor " + x);
+
+Calculo calc = new Calculo();
+calc.Double(x);
+
+Console.WriteLine("Valor do argumento x depois de passar por valor " + x);
+public class Calculo
+{
+    public void Double(int y)
+    {
+        y *= 2;
+        Console.WriteLine($"Valor do parâmetro y no método Dobrar {y}");
+    }
+}
