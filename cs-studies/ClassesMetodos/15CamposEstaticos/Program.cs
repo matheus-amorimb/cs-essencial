ee286162d373fe5df23ac913b502672baec04c2b
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-13 22:14:16 -0300

    update

---
+++ cs-studies/ClassesMetodos/15CamposEstaticos/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Cliente
+{
+    public static string? Nome;
+    public static int Idade;
+	
+    public Cliente(string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    } 
+}
