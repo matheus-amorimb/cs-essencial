1ec5eae0d96230b3c3b62097d8720eb87f6415eb
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-15 13:05:18 -0300

    update

--- cs-studies/ClassesMetodos/18Structs/Program.cs
+++ cs-studies/ClassesMetodos/18Structs/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Person p1 = new Person();
+
+p1.Display();
+public struct Person
+{
+    public string Name;
+    public int Age { get; set;}
+
+    public void Display()
+    {
+        Console.WriteLine("Struct");
+    }
+}
---
+++ cs-studies/ClassesMetodos/19Enum/Program.cs
@@ -0,0 +1,15 @@
+﻿using _19Enum;
+
+Console.WriteLine((DaysOfWeek.Monday));
+Console.WriteLine((DaysOfWeek.Sunday));
+
+int day1 = (int) DaysOfWeek.Monday;
+int day7 = (int) DaysOfWeek.Sunday;
+    
+Console.WriteLine($"Monday: {day1}");
+Console.WriteLine($"Sunday: {day7}");
+
+Console.WriteLine("----------");
+
+Console.WriteLine($"Accessing first item of Categories: {(Categories)0}");
+Console.WriteLine($"Accessing last item of Categories: {(Categories)5}");
