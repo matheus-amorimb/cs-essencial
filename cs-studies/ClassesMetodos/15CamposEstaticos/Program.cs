b86dd85f92739741b9dcf9961c2d6edfa622de5d
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-15 08:33:40 -0300

    update

--- cs-studies/ClassesMetodos/15CamposEstaticos/Program.cs
+++ cs-studies/ClassesMetodos/15CamposEstaticos/Program.cs
@@ -1,11 +1,24 @@
-﻿public class Cliente
+﻿BankAccount c1 = new BankAccount(1015, "Matheus");
+BankAccount c2 = new BankAccount(1020, "Beatriz");
+
+Console.WriteLine($"{BankAccount.Interest}");
+
+public class BankAccount
 {
-    public static string? Nome;
-    public static int Idade;
-	
-    public Cliente(string? nome, int idade)
+    public int Account;
+    public string? Name;
+    public static float Interest = 15;
+
+    public BankAccount(int account, string name)
     {
-        Nome = nome;
-        Idade = idade;
-    } 
+        Account = account;
+        Name = name;
+    }
+    
+    public float AnnualInterest()
+    {
+        return Interest * 12;
+    }
+    
 }
+
