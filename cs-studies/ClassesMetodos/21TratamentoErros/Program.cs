9b607a31c33cae91b5aef5ffb47cab3991f728dc
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-16 12:01:36 -0300

    update

---
+++ cs-studies/ClassesMetodos/21TratamentoErros/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Calculate the division of x by y");
+
+Console.WriteLine("\nInsert the value of x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nInsert the value of y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int z = x / y;
+    Console.WriteLine($"\n{x} / {y} = {z}");
+}
+catch
+{
+    Console.WriteLine("\nCannot divide by 0");
+}
+finally
+{
+    Console.WriteLine("\n Processing is done");
+}
---
+++ cs-studies/ClassesMetodos/22TiposAnonimos/Program.cs
@@ -0,0 +1,7 @@
+﻿var aluno = new
+{
+    Nome = "Maria",
+    Idade = 42,
+    Endereco = new {Id = 1, Cidade = "Santos", Pais = "Brasil"}
+};
+Console.WriteLine(aluno.Nome + " - " + aluno.Idade + "anos" + " - " + aluno.Endereco.Cidade);
