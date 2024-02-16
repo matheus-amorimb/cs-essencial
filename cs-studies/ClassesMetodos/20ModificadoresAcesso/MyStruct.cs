df03055f91100cf85a4b4d2e8210bc7e2ea54164
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-16 08:43:54 -0300

    update

---
+++ cs-studies/ClassesMetodos/20ModificadoresAcesso/MyStruct.cs
@@ -0,0 +1,13 @@
+namespace _20ModificadoresAcesso;
+
+internal struct MyStruct
+{
+    int MyField;
+
+    string? MyProperty { get; set; }
+
+    void MyMethod()
+    {
+        Console.WriteLine("My method");
+    }
+}
