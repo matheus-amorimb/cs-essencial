adfd793782a0fd0d0c68dbec06bac378eeb3675b
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-16 23:23:19 -0300

    update

--- cs-studies/ArrayList/1Array/Program.cs
+++ cs-studies/ArrayList/1Array/Program.cs
@@ -1,3 +1,12 @@
-﻿    // See https://aka.ms/new-console-template for more information
+﻿
+int [] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-Console.WriteLine("Hello, World!");
+var names = new string[5]{"Ana", "Maria", "Marta", "Paulo", "Carlos"};
+
+Console.WriteLine(numbers[0]);
+
+Console.WriteLine(names[0]);
+
+names[0] = "Matheus";
+
+Console.WriteLine(names[0]);
