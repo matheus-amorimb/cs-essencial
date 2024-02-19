db48a4e5b68c3e7cd143c2187503235b104a54db
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-19 16:33:01 -0300

    update

--- cs-studies/ArrayList/ExercicioArrayBidimensional/Program.cs
+++ cs-studies/ArrayList/ExercicioArrayBidimensional/Program.cs
@@ -5,7 +5,7 @@ for (int i = 0; i < students.GetLength(0); i++)
 {
     for (int j = 0; j < students.GetLength(1); j++)
     {
-        Console.Write($"Insira o aluno da linha {i} e coluna {j}: ");
+        Console.Write($"Informe o valor da posição [{i}, {j}]: ");
         students[i, j] = Console.ReadLine();
     }
 }
@@ -14,6 +14,6 @@ for (int i = 0; i < students.GetLength(0); i++)
 {
     for (int j = 0; j < students.GetLength(1); j++)
     {
-        Console.Write($"\nAluno linha {i} e coluna {j}: {students[i, j]}");
+        Console.Write($"[{i}, {j}]: {students[i, j]}\t");
     }
 }
---
+++ cs-studies/LeetCode/TestProblem03/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestProblem03;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
