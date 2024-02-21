e2cde299655853928919b72b8a3667d769d25f1c
Matheus Batista <mbatista.sarti@gmail.com>
2024-02-21 18:48:15 -0300

    update

---
+++ cs-studies/ArrayList/6List/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> list;
+list = new List<string>();
+
+List<int> list1 = new List<int>();
+var list2 = new List<int>();
+List<double> list3 = new();
+
+var list4 = new List<string>()
+{
+    "Matheus", "Maria", "Cris"
+}; 
+
