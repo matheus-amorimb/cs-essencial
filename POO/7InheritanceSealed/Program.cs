db50926f23367210bd8353d20d2014979c6b143e
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-13 16:11:01 -0300

    inserting new feat

---
+++ POO/7InheritanceSealed/Program.cs
@@ -0,0 +1,38 @@
+﻿class BaseClass
+{
+    protected virtual void Method1()
+    {
+        Console.WriteLine("BaseClass.Method1()");
+    }
+    
+    protected virtual void Method2()
+    {
+        Console.WriteLine("BaseClass.Method2()");
+    }
+}
+
+class Class1: BaseClass
+{
+    sealed protected override void Method1()
+    {
+        Console.WriteLine("Class1.Method1()");
+    }
+    
+    protected override void Method2()
+    {
+        Console.WriteLine("Class1.Method2()");
+    }
+}
+
+class Class2: Class1
+{
+    // protected override void Method1()
+    // {
+    //     Console.WriteLine("Class2.Method1()");
+    // }
+    
+    protected override void Method2()
+    {
+        Console.WriteLine("Class2.Method2()");
+    }
+}
