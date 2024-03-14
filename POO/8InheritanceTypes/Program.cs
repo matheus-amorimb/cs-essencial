740644f11bb077aa7a9594f27b177b67f8b14562
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-14 20:40:46 -0300

    inserting new feat

---
+++ POO/8InheritanceTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿Developer juniorDeveloper = new Developer();
+
+juniorDeveloper.Name = "Matheus";
+juniorDeveloper.Class = "Electrical Engineering";
+juniorDeveloper.ProgrammingLanguage = "C#, Python, Angular";
+
+juniorDeveloper.DisplayInfo();
+class Person
+{
+    public string Name;
+}
+class Student : Person
+{
+    public string Class;
+}
+
+class Developer : Student
+{
+    public string? ProgrammingLanguage;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("################## Information ##################");
+        Console.WriteLine($"Name: {this.Name}");
+        Console.WriteLine($"Class: {this.Class}");
+        Console.WriteLine($"Programming Language: {this.ProgrammingLanguage}");
+    }
+}
