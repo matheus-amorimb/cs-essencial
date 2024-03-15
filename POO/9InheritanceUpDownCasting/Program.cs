848be81149c812d4dcc6b1aff5d27d7e849b125c
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-14 21:14:47 -0300

    inserting new feat

---
+++ POO/9InheritanceUpDownCasting/Program.cs
@@ -0,0 +1,47 @@
+﻿Shape circle = new Circle(); //upcasting
+circle.Draw(); //do not have acess to PaintCircle
+
+Circle circle2 = new Circle();
+Shape shape = circle2; //implicity upcasting
+Circle c = (Circle)shape; //explicity downcasting
+
+c.Draw();
+c.PaintCircle();
+
+public class Shape
+{
+    protected int xpos, ypos;
+    
+    public Shape(){}
+
+    public Shape(int x, int y)
+    {
+        xpos = x;
+        ypos = y;
+    }
+
+    public virtual void Draw()
+    {
+        Console.WriteLine($"Draw shape at position: ({xpos}, {ypos})");
+    }
+    
+}
+
+public class Circle : Shape
+{
+    
+    public Circle() {}
+    
+    public Circle(int x, int y) : base(x, y){}
+
+    public override void Draw()
+    {
+        Console.WriteLine($"Draw circle at position: ({xpos}, {ypos})");
+    }
+
+    public void PaintCircle()
+    {
+        Console.WriteLine("Paint circle");
+    }
+    
+}
