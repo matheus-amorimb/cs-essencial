7f31f5d4f2438e6820c1fc221a2907383378902b
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-23 15:12:07 -0300

    :sparkles:

---
+++ Generics/04SortedDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿SortedDictionary<int, string> sortedDictionary = new SortedDictionary<int, string>()
+{
+    {50, "Brazil"},
+    {30, "China"},
+    {40, "USA"},
+    {10, "France"},
+    {20, "UK"},
+};
+
+Console.WriteLine("########## Displaying dictionary ##########");
+foreach (var item in sortedDictionary)
+{
+    Console.WriteLine($"key: {item.Key}; value: {item.Value}");
+}
---
+++ Generics/05HashSet/Program.cs
@@ -0,0 +1,16 @@
+﻿HashSet<int> numbers = new HashSet<int>{1, 2, 3, 4, 5};
+HashSet<string> fruits = new HashSet<string>{"uva", "banana", "pera"};
+HashSet<int> evenNumbers = new HashSet<int>();
+for(int i = 0; i < 6; i++)
+{
+    evenNumbers.Add(i * 2);
+}
+
+//Constructor using IEnumerable
+var odds = new int[] {1, 3, 5, 7, 9, 11, 13};
+HashSet<int> set = new HashSet<int>(odds);
+
+var spTeams = new HashSet<string>() {"Palmeiras", "São Paulo","Corinthians"};
+var rjTeams = new HashSet<string>() {"Vasco", "Flamengo","Fluminense"};
+var baTeams = new HashSet<string>() {"Bahia", "Viória","Juazeiro"};
+var clubWorldCupChampions = new HashSet<string>(){"Santos", "Palmeiras", "São Paulo", "Flamengo"};
