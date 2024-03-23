a673f5e9fea29486c02d11851e5d1fe5b7f2cc03
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-23 19:06:11 -0300

    :sparkles:

---
+++ Generics/07ReadOnlyCollection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+var planets = new List<string>(){"Earth", "Mars", "Mercury", "Jupiter"};
+
+ReadOnlyCollection<string> planetsList = new ReadOnlyCollection<string>(planets);
+var newPlanetsList = planets.AsReadOnly();
+
+foreach (var planet in planetsList)
+{
+    Console.Write($"{planet}; ");
+}
+
+Console.WriteLine($"\n\nNumber os planets in the collection: {planetsList.Count}");
+Console.WriteLine($"\nFirst planet in the collection: {planetsList[0]}");
+Console.Write($"\nIs Jupiter in the collection? ");
+Console.WriteLine(planetsList.Contains("Jupiter") ? "Yes" : "No");
+Console.WriteLine($"\nIndex of planet Earth: {planetsList.IndexOf("Earth")}");
+Console.WriteLine($"\nCopying the elements from collection to unidimensional array");
+string[] planetsArray = new string[planetsList.Count + 2];
+planetsList.CopyTo(planetsArray, 1);
+foreach (var planet in planetsArray)
+{
+    Console.WriteLine($"\"{planet}\"");
+}
