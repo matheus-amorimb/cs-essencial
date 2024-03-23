a1a0a86de402150fc96c9f45464a9735e31e6226
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-23 08:10:31 -0300

    :sparkles:

---
+++ Generics/02GetHashCodeEquals/Program.cs
@@ -0,0 +1,27 @@
+﻿string a = "Matheus";
+string b = "Matheus";
+
+int x = 100;
+int y = 100;
+
+Person person1 = new Person(a, x);
+Person person2 = new Person(b, y);
+
+Console.WriteLine("##### Equals #####");
+Console.WriteLine("Person1 and Person2 are equal?");
+Console.WriteLine(person1.Equals(person2));
+
+Console.WriteLine("\n##### GetHashCode #####");
+Console.WriteLine($"Person1: {person1.GetHashCode()}");
+Console.WriteLine($"Person2: {person2.GetHashCode()}");
+public class Person(string name, int id)
+{
+    public string Name { get; set; } = name;
+    public int Id { get; set; } = id;
+
+    public override bool Equals(object? obj)
+    {
+        var person = (Person)obj;
+        return (this.Name == person.Name && this.Id == person.Id);
+    }
+}
---
+++ Generics/Exercise02_GetHashCodeEquals/Program.cs
@@ -0,0 +1,38 @@
+﻿var person1 = new Person("Matheus","999.999.999-99");
+var person2 = new Person("Matheus","999.999.999-99");
+var person3 = new Person("Matheus","888.888.888-88");
+
+Console.WriteLine("##### Equals #####");
+Console.Write($"Person1 and Person2 are equal? {person1.Equals(person2)}");
+Console.Write($"\nPerson2 and Person3 are equal? {person2.Equals(person3)}");
+
+Console.WriteLine("\n\n##### GetHashCode #####");
+Console.WriteLine($"Person1: {person1.GetHashCode()}");
+Console.WriteLine($"Person2: {person2.GetHashCode()}");
+Console.WriteLine($"Person3 {person3.GetHashCode()}");
+
+public class Person(string name, string cpf)
+{
+    public string Name { get; set; } = name;
+    public string CPF { get; set; } = cpf;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (obj is not Person)
+        {
+            return false;
+        }
+        
+        var person = (Person)obj;
+        return (this.Name == person.Name && this.CPF == person.CPF);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.CPF.GetHashCode();
+    }
+}
