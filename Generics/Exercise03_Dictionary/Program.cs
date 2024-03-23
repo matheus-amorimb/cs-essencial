c438ae97245c117f7f02d756bc7e43696e14dae6
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-23 14:28:13 -0300

    :sparkles:

--- Generics/Exercise03_Dictionary/Program.cs
+++ Generics/Exercise03_Dictionary/Program.cs
@@ -6,17 +6,45 @@ students.Add(2, new Student("Ana", 9));
 students.Add(3, new Student("Alex", 6));
 students.Add(4, new Student("Diná", 5));
 
-Console.WriteLine("######## Students and Grades ########");
-foreach (var student in students)
+PrintStudents("students and grades", students);
+
+var mariaStudent = students.Where(item => item.Value.Name == "Maria").ToList();
+var mariaKey = mariaStudent[0].Key;
+students[mariaKey].Grade = 10;
+
+PrintStudents("Maria grade modified", students);
+
+var dinaStudent = students.Where(item => item.Value.Name == "Diná").ToList();
+var dinaKey = dinaStudent[0].Key;
+students.Remove(dinaKey);
+
+PrintStudents("Dina removed", students);
+
+students.Add(5, new Student("Vilma", 7));
+
+PrintStudents("Vilma inserted", students);
+
+var studentsOrderedByName = students.OrderBy(item => item.Value.Name).ToDictionary();
+
+PrintStudents("Dictionary ordered by name", studentsOrderedByName);
+
+students.Clear();
+
+PrintStudents("Dictionary cleaned up", students);
+
+static void PrintStudents(string message, Dictionary<int, Student> students)
 {
-    Console.WriteLine($"student: {student.Value.Name}; grade: {student.Value.Grade} ");
-    
+    Console.WriteLine($"\n######## {message} ########");
+    foreach (var student in students)
+    {
+        Console.WriteLine($"student: {student.Value.Name}; grade: {student.Value.Grade} ");
+    }
+    Console.WriteLine("#####################################\n");
 }
-
-
-
 public class Student(string name, int grade)
 {
     public string? Name { get; set; } = name;
     public int Grade { get; set; } = grade;
+    
 }
+
