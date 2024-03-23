30e744762a08dccfd4b734fb927d04969791ba4b
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-23 13:50:16 -0300

    :sparkles:

---
+++ Generics/03Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+dictionary.Add("Matheus", 25);
+dictionary.Add("Lohana", 29);
+dictionary.Add("Luan", 27);
+
+Console.WriteLine("#### Dictionary ####");
+foreach (var item in dictionary)
+{
+    Console.WriteLine($"key: {item.Key}; value: {item.Value}");
+}
+
+string message = dictionary.TryGetValue("Arthur", out int arthurAge) ? 
+   $"Arthur's age is {arthurAge} years old." : 
+   $"Arthur's age not found in the dictionary.";
+
+Console.WriteLine("\n##### Searching for a key in the dictionary #####");
+Console.WriteLine(message);
+
+Console.WriteLine("\n#### Dictionary Ordered####");
+var orderedDict = dictionary.OrderBy(item => item.Key);
+foreach (var item in orderedDict)
+{
+    Console.WriteLine($"key: {item.Key}; value: {item.Value}");
+}
---
+++ Generics/Exercise03_Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿Dictionary<int, Student> students = new Dictionary<int, Student>();
+
+students.Add(0, new Student("Maria", 7));
+students.Add(1, new Student("Eric", 8));
+students.Add(2, new Student("Ana", 9));
+students.Add(3, new Student("Alex", 6));
+students.Add(4, new Student("Diná", 5));
+
+Console.WriteLine("######## Students and Grades ########");
+foreach (var student in students)
+{
+    Console.WriteLine($"student: {student.Value.Name}; grade: {student.Value.Grade} ");
+    
+}
+
+
+
+public class Student(string name, int grade)
+{
+    public string? Name { get; set; } = name;
+    public int Grade { get; set; } = grade;
+}
