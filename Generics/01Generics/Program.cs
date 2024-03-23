630df817b8c7736d20b0f80dfc2c374f30c24cf7
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-23 07:22:34 -0300

    :sparkles:

---
+++ Generics/01Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿
+GenericClass<int, int> genericClass = new GenericClass<int, int>();
+
+int num1 = 10;
+int num2 = 10;
+
+float float1 = 10;
+float float2 = 10;
+
+string string1 = "10";
+string string2 = "10";
+
+//It would be necessary to create another class if I wanted to compare two floats
+// Test.Compare(float1, float2); 
+
+Test.CompareUsingGeneric(num1, num2);
+Test.CompareUsingGeneric(float1, float2);
+Test.CompareUsingGeneric(string1, string2);
+
+public class Test
+{
+    // public static bool Compare(int num1, int num2)
+    // {
+    //     var result = num1.Equals(num2);
+    //     return result;
+    // }
+
+    public static bool CompareUsingGeneric<T>(T value1, T value2)
+    {
+        var result = value1.Equals(value2);
+        Console.WriteLine("\n###################################################");
+        Console.WriteLine($"The types: {value1.GetType()} e {value2.GetType()}");
+        Console.WriteLine($"{value1} e {value2} are equal? {result}");
+        Console.WriteLine("###################################################");
+        return result;
+    } 
+    
+}
+
+public class GenericClass<T1, T2>
+{
+    public static bool Compare(T1 value1, T2 value2)
+    {
+        var result = value1.Equals(value2);
+        return result;
+    }
+}
---
+++ Generics/Exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿GenericClass<int> genericClass = new GenericClass<int>();
+
+Console.WriteLine("GenericClass<T> of int objects");
+for (int i = 0; i < 5; i++)
+{
+    genericClass[i] = (i + 1) * 10;
+    Console.Write(genericClass[i] + "; ");
+}
+
+public class GenericClass<T>
+{
+    public T[] Objects = new T[5];
+    // public List<T> Objects = new List<T>();
+    public int Indexer = 0;
+    
+    public void Add(T item)
+    {
+        if (Indexer < Objects.Length)
+        {
+            Objects[Indexer] = item;
+        }
+        else
+        {
+            Console.WriteLine("Your array is already full");
+        }
+        Indexer++;
+        // Objects.Add(item);
+    }
+    
+    //Indexer
+    public T this[int index]
+    {
+        get => Objects[index];
+        set => Objects[index] = value;
+    }
+}
