6c17cf058584d967c1474dbaaa290708cbc13425
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-25 07:00:43 -0300

    :feat:

---
+++ ErrorHandling/02ThrowException/Program.cs
@@ -0,0 +1,47 @@
+﻿try
+{
+    A.ProcessA();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Handling the error in main");
+    Console.WriteLine($"Exception message: {e.Message}");
+}
+class A
+{
+    public static void ProcessA()
+    {
+        try
+        {
+            B.ProcessB();
+        }
+        catch (Exception e)
+        {
+            throw;
+            // Console.WriteLine("Handling the error in A");
+        }
+    }
+}
+
+class B
+{
+    public static void ProcessB()
+    {
+        try
+        {
+            C.ProcessC();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
+
+class C
+{
+    public static void ProcessC()
+    {
+        throw new NotImplementedException("Method not implemented");
+    }
+}
---
+++ ErrorHandling/03FilterExceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.WriteLine("Insert the dividend");
+    int dividend = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInsert the denominator");
+    int denominator = Convert.ToInt32(Console.ReadLine());
+
+    double quotient = dividend / denominator;
+
+    Console.WriteLine($"\nThe result of division {dividend}/{denominator} is: {quotient}");
+
+}
+catch (DivideByZeroException) when (DayOfWeek.Friday == DayOfWeek.Friday)
+{
+    Console.WriteLine("\nIt's not possible to divide a number by zero, but do not worry. " +
+                      "It's friday, baby. Let's celebrate!!!");
+}
+catch (Exception e) when (e.Message.Contains("format"))
+{
+    Console.WriteLine("Filter with format exception:");
+    Console.WriteLine(e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
