15b48c318eaa80dddcd2add82bc9a089cb8d6904
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-03 08:51:30 -0300

    :feat:

---
+++ DelegatesLambdaEvents/02MulticastDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿
+MyDelegate myDelegate = new MyDelegate(Method1);
+
+myDelegate += Method2;
+myDelegate += Method3;
+myDelegate -= Method2;
+
+myDelegate.Invoke("Multicast Delegate");
+
+static void Method1(string message)
+{
+    Console.WriteLine($"Method1: {message}");
+}
+
+static void Method2(string message)
+{
+    Console.WriteLine($"Method2: {message}");
+}
+
+static void Method3(string message)
+{
+    Console.WriteLine($"Method3: {message}");
+}
+
+public delegate void MyDelegate(string message);
