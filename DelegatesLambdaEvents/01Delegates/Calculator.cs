3f3d454db03a1e92a41f6c37f6f89697591facad
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-03 08:26:29 -0300

    :feat:

---
+++ DelegatesLambdaEvents/01Delegates/Calculator.cs
@@ -0,0 +1,28 @@
+namespace _01Delegates;
+
+public class Calculator
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
--- DelegatesLambdaEvents/01Delegates/Program.cs
+++ DelegatesLambdaEvents/01Delegates/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using _01Delegates;
 
-Console.WriteLine("Hello, World!");
+CalculatorDelegate calculatorDelegate = new CalculatorDelegate(Calculator.Add);
+var result1 = calculatorDelegate.Invoke(20, 30);
+Console.WriteLine($"Add = {result1}");
+
+CalculatorDelegate calculatorDelegate2 = Calculator.Subtract;
+var result2 = calculatorDelegate.Invoke(20, 30);
+Console.WriteLine($"Subtract = {result2}");
+
+CalculatorDelegate calculatorDelegate3 = (double a, double b) => Calculator.Multiply(a, b);
+var result3 = calculatorDelegate.Invoke(20, 30);
+Console.WriteLine($"Subtract = {result3}");
+
+public delegate double CalculatorDelegate(double a, double b);
