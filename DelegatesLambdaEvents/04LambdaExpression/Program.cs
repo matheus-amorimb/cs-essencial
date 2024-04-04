1f3d258bd065585fad2d5b0de2b0bb9bccb2e1b8
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-04 20:52:09 -0300

    :feat:

---
+++ DelegatesLambdaEvents/04LambdaExpression/Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> names = new List<string>();
+names.Add("Matheus");
+names.Add("Pedro");
+names.Add("João");
+
+string result = names.Find(name => name.Equals("Matheus"));
+
+Console.WriteLine(result);
+
+
---
+++ DelegatesLambdaEvents/05DelegatePredicate/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 10;
+
+Predicate<int> delegateEven = x => x % 2 == 0;
+
+if (delegateEven(number))
+{
+    Console.WriteLine("The number is even");
+}
+else
+{
+    Console.WriteLine("The number is odd");
+}
