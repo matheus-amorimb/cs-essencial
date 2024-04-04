b2b970de3c42f758ddf39af32d33efb9088d72a5
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-04 20:09:37 -0300

    :feat:

---
+++ DelegatesLambdaEvents/03AnonymousMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿// Print print = delegate(int value)
+// {
+//     Console.WriteLine("Anonymous Method");
+//     Console.WriteLine($"value: {value}");
+// };
+//
+// print(1250);
+//
+//
+// public delegate void Print(int value);
+
+List<string> names = new List<string>();
+names.Add("Matheus");
+names.Add("Pedro");
+names.Add("João");
+
+string result = names.Find(delegate(string name)
+{
+    return name.Equals("Matheus");
+});
+
+Console.WriteLine($"{result}");
