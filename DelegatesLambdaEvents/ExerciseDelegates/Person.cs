987f6b00d33ab36ab0f80638412d2194d56cae2e
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-04 21:40:07 -0300

    :feat:

---
+++ DelegatesLambdaEvents/ExerciseDelegates/Person.cs
@@ -0,0 +1,7 @@
+namespace ExerciseDelegates;
+
+public class Person (string name, int age)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+}
---
+++ DelegatesLambdaEvents/ExerciseDelegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExerciseDelegates;
+
+List<Person> persons = new List<Person>()
+{
+    new Person("Alice", 12),
+    new Person("Bob", 17),
+    new Person("Charlie", 40),
+    new Person("Frank", 42),
+    new Person("Grace", 33)
+};
+Console.WriteLine("==================================================");
+Console.WriteLine("All Persons");
+Console.WriteLine("==================================================");
+Action<Person> printPersons =  x => Console.WriteLine($"Name: {x.Name}, Age: {x.Age}");
+
+foreach (Person person in persons)
+{
+    printPersons(person);
+}
+
+Console.WriteLine("\n==================================================");
+Console.WriteLine("Persons over 18");
+Console.WriteLine("==================================================");
+
+Predicate <Person> overEighteen = person => person.Age >= 18;
+List<Person> personsOverEighteen = persons.FindAll(person => overEighteen(person));
+
+foreach (Person person in personsOverEighteen)
+{
+    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+}
