12fe11607d470b9a183583fe991d6fd877c03f90
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-04 21:12:33 -0300

    :feat:

--- DelegatesLambdaEvents/06DelegateAction/Program.cs
+++ DelegatesLambdaEvents/06DelegateAction/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int number = 4;
+int number2 = 8;
+int number3 = 12;
 
-Console.WriteLine("Hello, World!");
+Action<int, int, int> square = 
+    (x, y, z) => 
+        Console.WriteLine($"x²: {Math.Pow(x, 2)} \ny²: {Math.Pow(y, 2)}, \nz²: {Math.Pow(z, 2)}");
+
+square(number, number2, number3);
---
+++ DelegatesLambdaEvents/07DelegateFunc/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//
+// static double SquareRoot(int number)
+// {
+//     return Math.Sqrt(number);
+// }
+
+int number = 8;
+
+Func<int, double> squareRoot = x => Math.Sqrt(x);
+
+Console.WriteLine(squareRoot(number));
