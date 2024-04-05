fc2f90cc6e18660214e1cd8a24c8001879989899
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-05 08:57:29 -0300

    :feat:

---
+++ DelegatesLambdaEvents/08Events/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Order order = new Order();
+
+//Subscribe subscribers
+order.onCreateOrder += Email.Send;
+order.onCreateOrder += Message.Send;
+
+order.CreateOrder();
+
+delegate void OrderEventHandler();
+
+//Publisher
+class Order
+{
+    public event OrderEventHandler? onCreateOrder;
+    public void CreateOrder()
+    {
+        Console.WriteLine("============================");
+        Console.WriteLine("Order created");
+        Console.WriteLine("============================");
+        if (onCreateOrder != null)
+        {
+            Console.WriteLine("\n============================");
+            Console.WriteLine("Event");
+            Console.WriteLine("============================");
+            onCreateOrder();
+        }
+    }
+    
+}
+
+//Subscribers
+class Email
+{
+    public static void Send()
+    {
+        Console.WriteLine("Sending a email");
+    }
+}
+
+class Message
+{
+    public static void Send()
+    {
+        Console.WriteLine("Sending a message");
+    }
+}
