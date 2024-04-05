6cb90d7a46e1a5194179d9e4763dfbb5beca9e23
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-05 10:24:03 -0300

    :feat:

---
+++ DelegatesLambdaEvents/09EventHandler/Program.cs
@@ -0,0 +1,46 @@
+﻿Order order = new Order();
+
+//Subscribe subscribers
+order.OnCreateOrder += Email.Send;
+order.OnCreateOrder += Message.Send;
+
+order.CreateOrder();
+
+delegate void OrderEventHandler();
+
+//Publisher
+class Order
+{
+    public event EventHandler? OnCreateOrder;
+    public void CreateOrder()
+    {
+        Console.WriteLine("============================");
+        Console.WriteLine("Order created");
+        Console.WriteLine("============================");
+        if (OnCreateOrder != null)
+        {
+            Console.WriteLine("\n============================");
+            Console.WriteLine("Event");
+            Console.WriteLine("============================");
+            OnCreateOrder(this, EventArgs.Empty);
+        }
+    }
+    
+}
+
+//Subscribers
+class Email
+{
+    public static void Send(object? sender, EventArgs eventArgs)
+    {
+        Console.WriteLine("Sending a email");
+    }
+}
+
+class Message
+{
+    public static void Send(object? sender, EventArgs eventArgs)
+    {
+        Console.WriteLine("Sending a message");
+    }
+}
---
+++ DelegatesLambdaEvents/10EventHandlerTEventArgs/Program.cs
@@ -0,0 +1,52 @@
+﻿Order order = new Order();
+
+//Subscribe subscribers
+order.OnCreateOrder += Email.Send;
+order.OnCreateOrder += Message.Send;
+
+order.CreateOrder("mabatista@", "(27)98888-8888");
+
+class OrderEventArgs : EventArgs
+{
+    public string? Email { get; set; }
+    public string? Message { get; set; }
+}
+
+//Publisher
+class Order
+{
+    public event EventHandler<OrderEventArgs> OnCreateOrder;
+    public void CreateOrder(string email, string message)
+    {
+        Console.WriteLine("============================");
+        Console.WriteLine("Order created");
+        Console.WriteLine("============================");
+        if (OnCreateOrder != null)
+        {
+            Console.WriteLine("\n============================");
+            Console.WriteLine("Event");
+            Console.WriteLine("============================");
+            OnCreateOrder(this, new OrderEventArgs(){Email = email, Message = message});
+        }
+    }
+}
+
+//Subscribers
+class Email
+{
+    public static void Send(object? sender, OrderEventArgs eventArgs)
+    {
+        Console.WriteLine($"Sending a email to {eventArgs.Email}");
+    }
+}
+
+class Message
+{
+    public static void Send(object? sender, OrderEventArgs eventArgs)
+    {
+        Console.WriteLine($"Sending a message to {eventArgs.Message}");
+    }
+}
+
+
+
