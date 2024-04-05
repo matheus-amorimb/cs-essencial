da379af88f5dd509fa9907c7b2f24de7b3433838
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-05 10:58:24 -0300

    :feat:

---
+++ DelegatesLambdaEvents/11ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿string name = "Matheus";
+
+string reversedString = name.ReverseString();
+
+Console.WriteLine(reversedString);
+
+public static class StringExtensions
+{
+    public static string ReverseString(this string str)
+    {
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
