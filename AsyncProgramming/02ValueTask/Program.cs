d4efc1c7eaa6efe2424a66642e40db644f06055a
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-06 16:23:43 -0300

    :feat:]

---
+++ AsyncProgramming/02ValueTask/Program.cs
@@ -0,0 +1,18 @@
+﻿await MethodWithoutReturnAsync();
+var result = await MethodReturnValueAsync(100);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Result: {result}");
+
+static async ValueTask MethodWithoutReturnAsync()
+{
+    Console.WriteLine("Method does not return any value.");
+    await Task.Delay(2000);
+}
+
+static async ValueTask<int> MethodReturnValueAsync(int value)
+{
+    Console.WriteLine("Method returns any value.");
+    await Task.Delay(2000);
+    return value * 2;
+}
