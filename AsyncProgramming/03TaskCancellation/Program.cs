d141c0acf0bfc93fa9374355ae04b8413429c619
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-07 09:36:31 -0300

    :feat:

--- AsyncProgramming/03TaskCancellation/Program.cs
+++ AsyncProgramming/03TaskCancellation/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+try
+{
+    cancellationTokenSource.Cancel();
+    var result = await LongDurationTask(100, 
+        cancellationTokenSource.Token);
+
+    Console.WriteLine(result);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Task aborted");
+    Console.WriteLine($"time expired after {stopwatch.Elapsed}");
+}
+
+static Task<int> LongDurationTask(int value,
+    CancellationToken cancellationToken = default)
+{
+    Console.WriteLine("Long duration task executed.");
+
+    Task<int> task = null;
+
+    task = Task.Run(() =>
+    {
+        int result = 0;
+        for (int i = 0; i < value; i++)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                throw new TaskCanceledException(task);
+            }
+            Thread.Sleep(50);
+            result += i;
+        }
+        return result;
+    });
+    return task;
+}
