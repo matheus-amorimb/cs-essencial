4ed62fb60e38a0063935224eadd2b0cc3f575663
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-08 07:16:18 -0300

    :feat:

---
+++ AsyncProgramming/05ErrorHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿
+await ThrowExceptionAsync();
+
+static async Task ThrowExceptionAsync()
+{
+    try
+    {
+        var firstTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("IndexOutOfRangeException");
+        });
+        await firstTask;
+        Console.WriteLine("First task is done.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Block catch executed");
+        Console.WriteLine(e.Message);    
+    }
+}
---
+++ AsyncProgramming/06MultipleExceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿await ThrowMultipleExceptionsAsync();
+
+static async Task ThrowMultipleExceptionsAsync()
+{
+    Task? tasks = null;
+    try
+    {
+        var firstTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("firstTask: IndexOutOfRangeException");
+        });
+        var secondTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("secondTask: IndexOutOfRangeException");
+        });
+
+        tasks = Task.WhenAll(firstTask, secondTask);
+        await tasks;
+    }
+    catch
+    {
+        Console.WriteLine("The following exceptions were raised:");
+        AggregateException allExceptions = tasks.Exception;
+        foreach (var e in allExceptions.InnerExceptions)
+        {
+            // Console.WriteLine(e.GetType().ToString());
+            Console.WriteLine(e.Message);
+        }
+    }
+}
