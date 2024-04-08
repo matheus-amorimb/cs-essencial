1c3e82c53cf06eeeae6625622481fc5955a8ed2b
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-08 19:47:40 -0300

    :feat:

---
+++ LINQ/01Introduction/Program.cs
@@ -0,0 +1,20 @@
+﻿IList<string> fruits = new List<string>(){"Banana", "Apple", "Orange", "Grape"};
+
+//QUERY SYNTAX
+var result = from f in fruits
+    where f.Contains('e')
+    select f;
+
+var result2 = fruits.Where(fruit => fruit.Contains('e'));
+
+Console.WriteLine("QUERY SYNTAX");
+foreach (var fruit in result)
+{
+    Console.WriteLine(fruit);
+}
+
+Console.WriteLine("\nMETHOD SYNTAX");
+foreach (var fruit in result2)
+{
+    Console.WriteLine(fruit);
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,7 @@
+﻿IList<string> fruits = new List<string>(){"Banana", "Apple", "Orange", "Grape"};
+
+var result = from f in fruits
+    where f.Contains('e')
+    select f;
+
+Console.WriteLine(result);
