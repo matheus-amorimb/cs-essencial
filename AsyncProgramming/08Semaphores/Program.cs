f794088868c5427c0c187eb8016f18906871fc1a
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-08 18:50:09 -0300

    :feat:

--- AsyncProgramming/08Semaphores/Program.cs
+++ AsyncProgramming/08Semaphores/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Semaphore semaphore = new Semaphore(3, 5);
+
+for (int i = 0; i <10; i++)
+{
+    Thread thread = new Thread(() => ProcessOperation(semaphore));
+    thread.Name = "Thread: " + i;
+    thread.Start();
+}
+
+static void ProcessOperation(Semaphore semaphore)
+{
+    semaphore.WaitOne();
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name} entered the critical section");
+    
+    Thread.Sleep(6000);
+    
+    semaphore.Release();
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name} was released.");
+}
---
+++ AsyncProgramming/09SemaphoreSlim/Program.cs
@@ -0,0 +1,22 @@
+﻿SemaphoreSlim semaphoreSlim = new SemaphoreSlim(4);
+
+for (int i = 0; i <= 6; i++)
+{
+    string threadName = "Thread" + i;
+    int waiting = 2 + 2 * i;
+    Thread thread = new Thread(() => AccessDataBase(semaphoreSlim, threadName, waiting));
+    thread.Start();
+}
+
+static void AccessDataBase(SemaphoreSlim semaphoreSlim, 
+    string name, int seconds)
+{
+    Console.WriteLine($"{name}: wait to access the database");
+    semaphoreSlim.Wait();
+    
+    Console.WriteLine($"{name}: was authorized to access the database.");
+    Thread.Sleep(TimeSpan.FromSeconds(seconds));
+    
+    Console.WriteLine($"{name}: has been completed.");
+    semaphoreSlim.Release();
+}
