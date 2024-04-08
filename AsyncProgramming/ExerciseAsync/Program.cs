c57a610e481850fa71dea45078bb0ef980eed0d0
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-08 09:02:25 -0300

    :feat:

---
+++ AsyncProgramming/ExerciseAsync/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+static async Task DownloadFile()
+{
+    var secTime = 10;
+    var miliTime = TimeSpan.FromSeconds(secTime);
+    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(miliTime);
+    
+    var url = "";
+    HttpClient httpClient = new HttpClient();
+
+    try
+    {
+        var response = httpClient.GetAsync(url, cancellationTokenSource.Token);
+        await response;
+    }
+    catch (OperationCanceledException e)
+    {
+        Console.WriteLine("OperationCanceledException");
+        Console.WriteLine(e.Message);
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("HttpRequestException");
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("GeneralException");
+        Console.WriteLine(e.Message);
+    }
+} 
