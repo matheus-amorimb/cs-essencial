98fb441f9b5333216ca61f7634186854abbe410f
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-08 18:05:25 -0300

    :feat:

--- AsyncProgramming/07AsyncStream/Program.cs
+++ AsyncProgramming/07AsyncStream/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿await foreach (var month in GenerateMonths())
+{
+    Console.WriteLine(month);
+} 
+
+static async IAsyncEnumerable<string> GenerateMonths()
+{
+    yield return "january";
+    yield return "february";
+    await Task.Delay(2000);
+    yield return "march";
+    yield return "april";
+    yield return "may";
+}
